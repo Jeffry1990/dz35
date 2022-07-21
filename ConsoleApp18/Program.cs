8f6b11a73c87cfb8c65646b85a5d0f90a9c5b5a7
Jeffry1990 <109137757+Jeffry1990@users.noreply.github.com>
2022-07-21 14:44:38 +0300

    Add files via upload

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите имя: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Введите символ: ");
+            string symbol = Console.ReadLine();
+
+            int additionalLength = 4;
+            string additionalSymbol = "";
+
+            for (int i = 1; i <= (name.Length + additionalLength); i ++ )
+            {
+                additionalSymbol += symbol;
+            }
+
+            Console.WriteLine(additionalSymbol);
+            Console.WriteLine(symbol + " " + name + " " + symbol);
+            Console.WriteLine(additionalSymbol);
+        }
+    }
+}
